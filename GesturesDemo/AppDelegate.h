9c2479912aab7805f112e32070984c788971eb93
Gabriel Theodoropoulos <gabrieltheodoropoulos@Gabriels-MacBook-Pro.local>
2014-08-22 11:20:30 +0300

    Initial Commit

---
+++ GesturesDemo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  GesturesDemo
+//
+//  Created by Gabriel Theodoropoulos on 8/22/14.
+//  Copyright (c) 2014 Appcoda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ GesturesDemo/SecondViewController.h
@@ -0,0 +1,13 @@
+//
+//  SecondViewController.h
+//  GesturesDemo
+//
+//  Created by Gabriel Theodoropoulos on 8/22/14.
+//  Copyright (c) 2014 Appcoda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+@end
